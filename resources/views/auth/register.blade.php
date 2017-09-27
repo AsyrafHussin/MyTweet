1b227014d00df71441ac836f6431a2f9a20223cb
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-28 07:18:35 +0800

    Add iveport to support responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>MyTweet | Signup</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <style>
