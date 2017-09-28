3adcf19bc2df0ee833ac6f07f510062eec77d765
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-28 08:01:49 +0800

    Add profile migration and relationship

---
+++ database/migrations/2017_09_27_235611_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('username');
+            $table->text('bio');
+            $table->string('location');
+            $table->string('website');
+            $table->string('photo');
+            $table->string('banner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
