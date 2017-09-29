e98da215cece42b1fd08945d1e31c0344a023d5b
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-29 10:29:25 +0800

    Remove jumbotron padding bottom

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>MyTweet | Signup</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     <style>
     body{
