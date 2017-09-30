f8993b486fbddf99753ae16366ece31ad80367cb
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-30 12:30:25 +0800

    Fix login jumbotron width

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,29 +18,33 @@
 
 @section('content')
   <div class="container container-padding">
-      <div class="jumbotron">
-        <div class="container container-padding login-content">
-          <p class="login-title">Log in to MyTweet</p>
-          <form>
-              <div class="row">
-                  <div class="col-md-6">
-                    <input type="text" placeholder="Email or username" class="form-control input-login" >
-                    <br>
-                    <input type="text" placeholder="Password" class="form-control input-login">  
-                  </div>
-              </div><br>
-              <button class="btn btn-info btn-tweet login-margin">Log in</button>
-              <label class="login-helpe login-helper-black" class="checkbox-inline"><input type="checkbox" value=""> Remember me</label>
-              <span class="separator">&middot;</span>
-              <a class="primary-link login-helper" href="#">Forgot password?</a>
-              <br><br>
-              <p class="login-helper">New to MyTweet? 
-                  <a class="primary-link" href="signup">Sign up now »</a>
-              </p>
-              <p class="login-helper">Already using MyTweet via text message? 
-                  <a class="primary-link" href="#">Activate your account »</a>
-              </p>
-          </form>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 col-lg-10 col-lg-offset-1">
+          <div class="jumbotron">
+            <div class="container container-padding login-content">
+              <p class="login-title">Log in to MyTweet</p>
+              <form>
+                  <div class="row">
+                      <div class="col-md-6">
+                        <input type="text" placeholder="Email or username" class="form-control input-login" >
+                        <br>
+                        <input type="text" placeholder="Password" class="form-control input-login">  
+                      </div>
+                  </div><br>
+                  <button class="btn btn-info btn-tweet login-margin">Log in</button>
+                  <label class="login-helpe login-helper-black" class="checkbox-inline"><input type="checkbox" value=""> Remember me</label>
+                  <span class="separator">&middot;</span>
+                  <a class="primary-link login-helper" href="#">Forgot password?</a>
+                  <br><br>
+                  <p class="login-helper">New to MyTweet? 
+                      <a class="primary-link" href="signup">Sign up now »</a>
+                  </p>
+                  <p class="login-helper">Already using MyTweet via text message? 
+                      <a class="primary-link" href="#">Activate your account »</a>
+                  </p>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
   </div>
