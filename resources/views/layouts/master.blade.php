b6c5d608244e2547d7f54a0c7730e465ea239436
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-10-01 12:42:44 +0800

    Include font in master page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
     <title>MyTweet | {{ $title }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/1.7.22/css/materialdesignicons.min.css">
 </head>
 <body @if($title=='Register') class="white-body" @endif>
     @yield("navigation")
